2b86e7a7502e718baa1f84c6901c92bc051969ed
Ambrose Li <ambrose.li@gmail.com>
2013-02-10 04:13:09 -0500

    Call the new dots_in_string function

--- foo.php
+++ foo.php
@@ -3,19 +3,23 @@
 /* vim: set filetype=php: */
 
 require_once 'a2b.inc';
+require_once 'b2d.inc';
 require_once 'messages.inc';
 
 ob_start('mb_output_handler');
 
 $t = new PigeonUEB(1);
+$d = new PigeonDots();
 
 foreach (read_messages() as $data) {
     $seq = $data[0];
     $quote = $data[1];
     $attribution = $data[2];
     $message = sprintf(($attribution? '“%s” – %s': '%s'), $quote, $attribution);
+    $braille = $t->translate_to_braille($message);
+    $dots = $d->dots_in_string($braille);
     print "$message\n";
-    print $t->translate_to_braille($message) . "\n";
+    print "$braille\n";
 } /* foreach */
 
 
