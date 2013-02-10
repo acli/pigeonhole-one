f6b3a36d7446f72702d8a7a2cdf10172a962be52
Ambrose Li <ambrose.li@gmail.com>
2013-02-10 00:34:40 -0500

    Split the messages.txt reading logic into its own file

--- foo.php
+++ foo.php
@@ -3,34 +3,7 @@
 /* vim: set filetype=php: */
 
 include 'a2b.inc';
-
-function read_messages() {
-    $input = 'messages.txt';
-    $it = array();
-    $h = fopen($input, 'r');
-    $re_data_line = '/^(\d+):(.+?)(?::([^:]*))?$/u';
-    $re_comment_line = '/^\s*(?:[#;][^\t]*)?$/u';
-    if ($h) {
-        $lc = 0;
-        for (;;) {
-            $s = fgets($h);
-        if ($s === false) { break; }
-            $lc += 1;
-            $s = rtrim($s);
-            if (preg_match($re_data_line, $s, $matches)) {
-                array_push($it, array($matches[1], $matches[2], $matches[3]));
-            } elseif (preg_match($re_comment_line, $s)) {
-                ;
-            } else {
-                error_log("$input:$lc: Malformed data line");
-            } /* if */
-        } /* for */
-    } else {
-        throw new Exception("Cannot open input file \"$input\"");
-    }
-    fclose($h);
-    return $it;
-} /* read_messages */
+include 'messages.inc';
 
 ob_start('mb_output_handler');
 
