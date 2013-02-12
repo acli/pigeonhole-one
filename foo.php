1d34dae4d21aec2f6a00799254e74947c96fe938
Ambrose Li <ambrose.li@gmail.com>
2013-02-12 18:46:13 -0500

    Pulled the window description code into windows.inc

--- foo.php
+++ foo.php
@@ -6,79 +6,27 @@ require_once 'a2b.inc';
 require_once 'b2d.inc';
 require_once 'messages.inc';
 require_once 'scan_order.inc';
+require_once 'windows.inc';
 require_once 'en_words.inc';
 
 $t = new PigeonUEB(1);
 $d = new PigeonDots;
 $iter = new PigeonScanOrder(PigeonScanOrder::SCAN_ORDER_BRAILLE());
 
-$number_of_windows = 1;
+$number_of_windows = 3;
 $estimated_time_needed_for_state_change = 5;
 
-function movement_descriptor($dots, $direction_label) {
-    global $number_of_windows;
+$win = new PigeonWindows($number_of_windows);
 
-    $message = '';
-    if (count($dots) == 1) {
-	if ($number_of_windows == 1) {
-	    $blind_label = 'the blind';
-	} else {
-	    $blind_label = 'blind ' . PigeonWords::numeral($dots[0]);
-	} /* if */
-	$message = sprintf('%s is going %s',
-		$blind_label, $direction_label);
-    } elseif (count($dots) == 2) {
-	$message = sprintf('blinds %s and %s are going %s',
-		PigeonWords::numeral($dots[0]),
-		PigeonWords::numeral($dots[1]),
-		$direction_label);
-    } elseif (count($dots) > 2) {
-	for ($i = 0; $i < count($dots) - 1; $i += 1) {
-	    if ($i > 0) {
-		$message .= ', ';
-	    } /* if */
-	    $message .= PigeonWords::numeral($dots[$i]);
-	} /* for */
-	$message = sprintf('blinds %s and %s are going %s',
-		$message,
-		PigeonWords::numeral($dots[count($dots) - 1]),
-		$direction_label);
-    } /* if */
-    return $message;
-}
-
-function describe_state($s0) {
-    ;
-} /* describe_state */
+function describe_progress($debug_pos, $debug_end, $t_0) {
+    $debug_progress = $debug_pos/$debug_end;
+    $debug_progress_percent = floor(100*$debug_pos/$debug_end);
+    $debug_eta = $debug_pos? PigeonWords::time_qty_si((time() - $t_0)/$debug_progress*(1 - $debug_progress)): 'unknown';
+} /* describe_progress */
 
 function describe_blind_movements($movements) {
-    global $number_of_windows;
-
-    $ups = array();
-    $downs = array();
-
-    for ($i = 0; $i < count($movements); $i += 1) {
-	if ($movements[$i] < 0) {
-	    array_push($ups, $i + 1);
-	} elseif ($movements[$i] > 0) {
-	    array_push($downs, $i + 1);
-	} /* if */
-    } /* for */
-
-    $ups_message = movement_descriptor($ups, 'up');
-    $downs_message = movement_descriptor($downs, 'down');
-
-    if ($ups_message && $downs_message) {
-	printf("Narrator: %s, and %s.\n", ucfirst($ups_message), $downs_message);
-    } elseif ($ups_message) {
-	printf("Narrator: %s.\n", ucfirst($ups_message));
-    } elseif ($downs_message) {
-	printf("Narrator: %s.\n", ucfirst($downs_message));
-    } else {
-	var_dump($number_of_windows);
-	printf("Narrator: %s not moving.\n",
-		($number_of_windows == 1? 'The blind is': 'Blinds are'));
-    } /* if */
+    global $win;
+    print "Narrator: " . $win->describe_blind_movements($movements) . "\n";
 } /* describe_blind_movements */
 
 function describe_braille_cell($dots_in_cell) {
@@ -136,12 +84,9 @@ foreach (read_messages() as $data) {
     print "% message: $braille ($message)\n";
 
     foreach ($dots as $dots_in_cell) {
-	$debug_progress = $debug_pos/$debug_end;
-	$debug_progress_percent = floor(100*$debug_pos/$debug_end);
-	$debug_eta = $debug_pos? PigeonWords::time_qty_si((time() - $t_0)/$debug_progress*(1 - $debug_progress)): 'unknown';
+	describe_progress($debug_pos, $debug_end, $t_0);
 	print "% progress: $debug_pos/$debug_end ($debug_progress_percent%), eta: $debug_eta\n";
 
-	describe_state($state);
 	$delta = $d->diff_matrix($state, $dots_in_cell);
 	print "% intent: [" . join(', ', $d->expand($state)) . '] -> [' . join(', ', $d->expand($dots_in_cell)) . "]\n";
 	print "% delta = [" . join(', ', $delta) . "]\n";
