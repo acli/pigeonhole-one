8e3fd13d9b1995fc9c6dfe2f972f619bc325ddd0
Ambrose Li <ambrose.li@gmail.com>
2013-02-11 20:36:59 -0500

    Slightly more realistic simulation

--- foo.php
+++ foo.php
@@ -5,9 +5,22 @@
 require_once 'a2b.inc';
 require_once 'b2d.inc';
 require_once 'messages.inc';
+require_once 'scan_order.inc';
 
 $t = new PigeonUEB(1);
-$d = new PigeonDots();
+$d = new PigeonDots;
+$iter = new PigeonScanOrder(PigeonScanOrder::SCAN_ORDER_BRAILLE());
+
+$number_of_windows = 3;
+$estimated_time_needed_for_state_change = 5;
+
+function describe_state($s0) {
+    ;
+} /* describe_state */
+
+ob_implicit_flush(TRUE);
+
+$state = array();
 
 foreach (read_messages() as $data) {
     $seq = $data[0];
@@ -20,12 +33,43 @@ foreach (read_messages() as $data) {
     $t_0 = time();
 
     foreach ($dots as $dots_in_cell) {
-	sleep(5);
+	describe_state($state);
+	$delta = $d->diff_matrix($state, $dots_in_cell);
+	print "% intent: [" . join(', ', $d->expand($state)) . '] -> [' . join(', ', $d->expand($dots_in_cell)) . "]\n";
+	print "% delta = [" . join(', ', $delta) . "]\n";
+	for (
+	    $i = $iter->rewind(), $j = 0, $k = 0;
+	    $i = $iter->valid();
+	    $iter->next(), $j += 1, $k = ($k + 1) % $number_of_windows
+	) {
+	    $p = $iter->scan_order[$j] - 1; # braille cell numbers are 1-based
+	    print "% DEBUG: j=$j, k=$k; p=$p\n";
+	    if ($k == 0) {
+		$movements = array($delta[$p]);
+	    } else {
+		array_push($movements, $delta[$p]);
+	    }
+	    if ($j + 1 == 6 || $k + 1 == $number_of_windows) {
+		if (in_array(1, $movements) || in_array(-1, $movements)) {
+		    for ($w = 0; $w < $number_of_windows; $w += 1) {
+			if ($movements[$w] > 0) {
+			    printf("You see blind %d coming down\n", $w + 1);
+			} elseif ($movements[$w] < 0) {
+			    printf("You see blind %d going up\n", $w + 1);
+			} /* if */
+		    } /* for */
+		} else {
+		    print "The blinds do not seem to be moving.\n";
+		} /* if */
+		sleep($estimated_time_needed_for_state_change);
+	    } /* if */
+	} /* for */
 	if (count($dots_in_cell) > 0) {
 	    print join(', ', $dots_in_cell) . "\n";
 	} else {
 	    print "blank\n";
 	} /* if */
+	$state = $dots_in_cell;
     } /* foreach */
 
     print "$braille\n";
