c5f70eacc17cf6de0e7cfb41e7a1b2085f8f3f47
Ambrose Li <ambrose.li@gmail.com>
2013-02-12 14:57:46 -0500

    Tests for numeral(). Currently failing, of course.

---
+++ tests/en_words.php
@@ -0,0 +1,41 @@
+<?php
+/* vi: set sw=4 ai sm: */
+/* vim: set filetype=php: */
+
+require_once('simpletest/autorun.php');
+require_once 'en_words.inc';
+
+class TestEnglishWords extends UnitTestCase {
+
+    function test_1() {
+	$this->assertEqual(PigeonWords::numeral(1), 'one');
+    }
+
+    function test_2() {
+	$this->assertEqual(PigeonWords::numeral(2), 'two');
+    }
+
+    function test_3() {
+	$this->assertEqual(PigeonWords::numeral(3), 'three');
+    }
+
+    function test_4() {
+	$this->assertEqual(PigeonWords::numeral(4), 'four');
+    }
+
+    function test_5() {
+	$this->assertEqual(PigeonWords::numeral(5), 'five');
+    }
+
+    function test_6() {
+	$this->assertEqual(PigeonWords::numeral(6), 'six');
+    }
+
+    function test_unimplemented() {
+	$this->expectException(-123456789);
+	PigeonWords::numeral('not a number!');
+    }
+
+}
+
+?>
